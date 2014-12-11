7f7473f4333ab6f51711846d25cc3fffc192c6a5
Martino Facchin <martino.facchin@si14.com>
2014-12-11 22:20:04 +0000

    cleanup code and export pulseInLong function

--- hardware/arduino/avr/cores/arduino/wiring_pulse.c
+++ hardware/arduino/avr/cores/arduino/wiring_pulse.c
@@ -27,21 +27,13 @@
 
 uint16_t (*countPulseASM)(const uint8_t port, const uint8_t bit, uint16_t maxloops, uint8_t state);
 
-uint16_t __attribute__((optimize("O0"))) count_pulse(uint8_t port, uint8_t bit, uint8_t stateMask, uint16_t maxloops)
-{
-	uint16_t tmp = 0;
-	while ((*portInputRegister(port) & bit) == stateMask) {
-		if (tmp++ == maxloops) {
-			return 0;
-		}
-	}
-	return (tmp);
-}
-
 /* Measures the length (in microseconds) of a pulse on the pin; state is HIGH
  * or LOW, the type of pulse to measure.  Works on pulses from 2-3 microseconds
  * to 3 minutes in length, but must be called at least a few dozen microseconds
- * before the start of the pulse. */
+ * before the start of the pulse.
+ *
+ * This function performs better with short pulses in noInterrupt() context
+ */
 unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
 {
 	// cache the port and bit of the pin in order to speed up the
@@ -84,10 +76,43 @@ unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
 		if (numloops++ == maxloops)
 			return 0;
 
-#if 1
 	width = countPulseASM(*portInputRegister(port), bit, maxloops, stateMask);
 	return clockCyclesToMicroseconds(width * 11 + 16);
-#else
+}
+
+/* Measures the length (in microseconds) of a pulse on the pin; state is HIGH
+ * or LOW, the type of pulse to measure.  Works on pulses from 2-3 microseconds
+ * to 3 minutes in length, but must be called at least a few dozen microseconds
+ * before the start of the pulse.
+ *
+ * ATTENTION:
+ * this function relies on micros() so cannot be used in noInterrupt() context
+ */
+unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout)
+{
+	// cache the port and bit of the pin in order to speed up the
+	// pulse width measuring loop and achieve finer resolution.  calling
+	// digitalRead() instead yields much coarser resolution.
+	uint8_t bit = digitalPinToBitMask(pin);
+	uint8_t port = digitalPinToPort(pin);
+	uint8_t stateMask = (state ? bit : 0);
+	unsigned long width = 0; // keep initialization out of time critical area
+
+	// convert the timeout from microseconds to a number of times through
+	// the initial loop; it takes 16 clock cycles per iteration.
+	unsigned long numloops = 0;
+	unsigned long maxloops = microsecondsToClockCycles(timeout);
+
+	// wait for any previous pulse to end
+	while ((*portInputRegister(port) & bit) == stateMask)
+		if (numloops++ == maxloops)
+			return 0;
+
+	// wait for the pulse to start
+	while ((*portInputRegister(port) & bit) != stateMask)
+		if (numloops++ == maxloops)
+			return 0;
+
     unsigned long start = micros();
     // wait for the pulse to stop
     while ((*portInputRegister(port) & bit) == stateMask) {
@@ -95,5 +120,4 @@ unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
             return 0;
     }
     return micros() - start;
-#endif
 }
