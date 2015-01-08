d9c0503488d5aeb31f67363bed366f20787dffbf
Martino Facchin <martino.facchin@si14.com>
2015-01-08 01:49:26 +0000

    Revert "pulseIn: export PORTA"
    This reverts commit 1e869e1e7e5474314425abb737bcb35205189d06.

--- hardware/arduino/avr/cores/arduino/wiring_private.h
+++ hardware/arduino/avr/cores/arduino/wiring_private.h
@@ -43,7 +43,6 @@ extern "C"{
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-uint16_t countPulseASM_A(const uint8_t port, const uint8_t bit, uint16_t maxloops, uint8_t state);
 uint16_t countPulseASM_B(const uint8_t port, const uint8_t bit, uint16_t maxloops, uint8_t state);
 uint16_t countPulseASM_C(const uint8_t port, const uint8_t bit, uint16_t maxloops, uint8_t state);
 uint16_t countPulseASM_D(const uint8_t port, const uint8_t bit, uint16_t maxloops, uint8_t state);
