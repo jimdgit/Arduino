48cf55a038fa2961471b1e86f835f3de69f84ab3
Martino Facchin <m.facchin@arduino.cc>
2015-03-18 17:53:16 +0100

    add stup parallel build

--- check_all_example_sketch.sh
+++ check_all_example_sketch.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 
-compile_sketch() {
+active_jobs=0
+
+compile_board() {
 #args:
 #$1 board
-#$2 file
-#$3 log_file
-echo test
+#$2 libs
+#$3 log_file_basename
+
+let active_jobs=active_jobs+1
+echo "==========================" >> $3_$1
+echo "Starting with board $1" >> $3_$1
+echo "==========================" >> $3_$1
+
+local file
+for file in `find $2 | grep "\.ino" | grep -v "inoflag"`
+do
+	#divide tests into $CPU_COUNT works
+	`$HOME_DIR/build/linux/work/arduino --board arduino:avr:$1 --verify $file > /dev/null 2>&1`
+	local RESULT=$?
+	echo "$file <-> $board in progress"
+	FILENAME=`basename $file`
+	if (($RESULT == 0)); then
+		echo "PASSED: $FILENAME" >> $3_$1
+	else
+		echo "ERROR $RESULT: $FILENAME" >> $3_$1
+	fi
+done
+let active_jobs=active_jobs-1
 }
 
 HOME_DIR=`pwd`
@@ -18,7 +40,7 @@ AVR_BOARDS="uno, diecimila, mega, nano, leonardo, yun, megaADK, micro, esplora, 
 SAM_BOARDS="arduino_due_x"
 
 git reset HEAD --hard
-git clean -dxf
+#git clean -dxf
 git checkout master
 #git pull origin master
 
@@ -31,33 +53,21 @@ echo `git log | head -1` >> $RESULT_FILE
 
 #EXAMPLES_INO=`find $AVR_LIB_DIRS | grep "\.ino" | grep -v "inoflag"`
 
+CPU_NUM=`nproc`
+
 IFS=', ' read -a avr_board_array <<< "$AVR_BOARDS"
 
 cd "$HOME_DIR/build/"
 ant clean
 ant
-ant run
 
 for board in "${avr_board_array[@]}"
 do
-
-echo "==========================" >> $RESULT_FILE
-echo "Starting with board $board" >> $RESULT_FILE
-echo "==========================" >> $RESULT_FILE
-
-
-for file in `find $AVR_LIB_DIRS | grep "\.ino" | grep -v "inoflag"`
-do
-	#divide tests into $CPU_COUNT works
-	`$HOME_DIR/build/linux/work/arduino --board arduino:avr:$board --verify $file > /dev/null 2>&1`
-	RESULT=$?
-	echo "$file <-> $board in progress"
-	FILENAME=`basename $file`
-	if (($RESULT == 0)); then
-		echo "PASSED: $FILENAME" >> $RESULT_FILE
+	echo "active_jobs: $active_jobs"
+	if [ $active_jobs -lt $CPU_NUM ]; then
+		compile_board $board $AVR_LIB_DIRS $RESULT_FILE &
 	else
-		echo "ERROR $RESULT: $FILENAME" >> $RESULT_FILE
+		sleep 10
 	fi
-done
 done
 
